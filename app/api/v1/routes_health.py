b08c704f3bf41889eb513f63b787119340ef49b4
John Silvestre <johnkerlin52@gmail.com>
2025-09-30 12:35:52 -0400

    feat: change message

--- app/api/v1/routes_health.py
+++ app/api/v1/routes_health.py
@@ -4,4 +4,4 @@ router = APIRouter()
 
 @router.get("/health", tags=["health"])
 def health():
-    return {"status": "ok", "message": "Service is healthy"}
+    return {"status": "ok", "message": "Service esta corriendo correctamente..."}
