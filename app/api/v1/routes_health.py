ef98188a28b6633eda1870999200aea676033492
John Silvestre <johnkerlin52@gmail.com>
2025-09-30 14:36:57 -0400

    feat: create endpoint ok

--- app/api/v1/routes_health.py
+++ app/api/v1/routes_health.py
@@ -5,3 +5,7 @@ router = APIRouter()
 @router.get("/health", tags=["health"])
 def health():
     return {"status": "ok", "message": "Service esta corriendo correctamente..."}
+
+@router.get("/ok", tags=["ok"])
+def ok():
+    return {"message": "Api en funcionamiento..."}
